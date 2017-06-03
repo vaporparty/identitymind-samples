5fcec235b3464e18f2f2ffe666a4db1819b26690
Kieran Sherlock <kieran@identitymind.com>
2017-06-02 17:22:15 -0700

    Add a link to the swagger documentation site

--- identity-graph-intelligence/routes/index.js
+++ identity-graph-intelligence/routes/index.js
@@ -12,13 +12,14 @@ var router = express.Router();
 //noinspection JSUnusedLocalSymbols
 router.get('/', function(req, res, next) {
     var html = "<html><body>" +
-               "<title>Graph Server</title>" +
+               "<title>IdentityLink Graph Server</title>" +
                "<div style='text-align:center'><img src='https://www.identitymindglobal.com/hubfs/IdentityMind_Global_theme/images/IDM_logo_tagline.png?t=1494635072687' width='300px' height='auto'/></div>" +
-               "<h2>Graph Server Example Code</h2>" +
+               "<h2>IdentityLink Graph Example Code</h2>" +
                "<h3><a href='/graph.html'>graph view</a></h3>" +
                "<h3><a href='/transfers.html'>transfer view</a></h3>" +
                "<h3><a href='/identity.html'>identity view</a></h3>" +
-               "<a href='/im/config'>configuration</a></h3>" +
+               "<a href='/im/config'>configuration</a><p>" +
+               "<a href='https://sandbox.identitymind.com/api/' target=\"_blank\">API Documentation</a><p>" +
                "</body></html>";
     res.send(html)
 });
